da8459301055c9411eebd071349979ebb83dd15f
Julius Lassen <julius5517@gmail.com>
2022-11-03 17:45:00 +0100

    Add FileIO, readData, setupMovies, setupSeries

---
+++ src/FileIO.java
@@ -0,0 +1,69 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class FileIO {
+
+    public ArrayList<String> readData(File path){
+        File file = path;
+        ArrayList<String> media = new ArrayList<>();
+        try {
+            Scanner input = new Scanner(file);
+            input.nextLine();
+
+            while(input.hasNextLine());{
+                media.add(input.nextLine());
+            }
+        } catch (FileNotFoundException e){
+            media = null;
+        }
+        return media;
+    }
+
+    public ArrayList<Movie> setupMovies(){
+        ArrayList<String> data = readData(new File("data/movies.txt"));
+        ArrayList<Movie> movies = new ArrayList<>();
+        for (String s: data) {
+            String[] values = s.split("; ");
+
+            String name = values[0];
+            int year = Integer.parseInt(values[1]);
+
+            ArrayList<String> genres = new ArrayList<>();
+            String[] genreArr = values[2].split(", ");
+            genres.addAll(Arrays.asList(genreArr));
+
+            float rating = Float.parseFloat(values[3]);
+
+            movies.add(new Movie(name, year, genres, rating));
+        }
+        return movies;
+    }
+
+    public ArrayList<Series> setupSeries(){
+        ArrayList<String> data = readData(new File("data/series.txt"));
+        ArrayList<Series> series = new ArrayList<>();
+        for (String s: data){
+            String[] values = s.split("; ");
+
+            String name = values[0];
+            int year = Integer.parseInt(values[1]);
+
+            ArrayList<String> genres = new ArrayList<>();
+            String[] genreArr = values[2].split(", ");
+            genres.addAll(Arrays.asList(genreArr));
+
+            float rating = Float.parseFloat(values[3]);
+
+            ArrayList<String> seasons = new ArrayList<>();
+            String[] seasonArr = values[4].split(", ");
+            seasons.addAll(Arrays.asList(seasonArr));
+
+            series.add(new Serie(name, year, genres,rating, seasons));
+        }
+        return series;
+    }
+
+}
