ed402e039ff714346968c00dcb11599d80ecb480
artiecodes <bjarke.dehlie@gmail.com>
2022-11-18 16:00:03 +0100

    working setupSeries

--- src/DBIO.java
+++ src/DBIO.java
@@ -30,9 +30,48 @@ public class DBIO implements IO {
         catch (Exception e) {
             e.printStackTrace();
         }
+        //System.out.println(arr);
+
+        return arr;
+    }
+
+    public static ArrayList<Series> setupSeries() {
+        ArrayList<Series> arr = new ArrayList<>();
+
+        String query = "SELECT * FROM series";
+        getConnection();
+        try {
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.execute(query);
+            ResultSet rs = statement.getResultSet();
+
+            while (rs.next()) {
+                String[] genreArr = rs.getString(4).trim().split(",");
+                ArrayList<String> genres = new ArrayList<>(Arrays.asList(genreArr));
+
+                String[] seasonArr = rs.getString(6).trim().split(", ");
+                ArrayList<String> seasons = new ArrayList<>(Arrays.asList(seasonArr));
+
+                String[] years = rs.getString(3).trim().split("-");
+                int yearStart = Integer.parseInt(years[0].trim());
+                int yearEnd = 0;
+                if(years.length > 1)
+                    yearEnd = Integer.parseInt(years[1].trim());
+
+                Series series = new Series (rs.getInt(1),rs.getString(2), rs.getFloat(5), genres, seasons, yearStart, yearEnd);
+                arr.add(series);
+            }
+            statement.close();
+
+            connection.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
         System.out.println(arr);
 
         return arr;
+
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         DBIO.setupMovies();
+        DBIO.setupSeries();
         Streaming stream = Streaming.getInstance();
         StreamUI ui = new StreamUI();
 
